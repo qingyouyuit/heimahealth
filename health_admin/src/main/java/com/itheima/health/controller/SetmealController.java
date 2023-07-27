d68b1e125587d8d85321de77dec523ff0ce67b31
qingyouyu <19863628661@163.com>
2023-07-27 21:26:47 +0800

    项目第二天-优化代码，使用Minio完成图片上传功能

--- health_admin/src/main/java/com/itheima/health/controller/SetmealController.java
+++ health_admin/src/main/java/com/itheima/health/controller/SetmealController.java
@@ -1,7 +1,10 @@
 package com.itheima.health.controller;
 
+import com.heima.file.service.FileStorageService;
 import com.itheima.health.common.PageParam;
+import com.itheima.health.common.R;
 import com.itheima.health.common.ResultPageData;
+import com.itheima.health.constant.MessageConstant;
 import com.itheima.health.service.impl.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @Slf4j
@@ -16,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SetmealController {
     @Autowired
     private SetmealService setmealService;
+    @Autowired
+    private FileStorageService fileStorageService;
 
     /**
      * 功能描述: 套餐模块分页查询
@@ -25,6 +34,34 @@ public class SetmealController {
     @PostMapping("/findPage")
     public ResultPageData findSetmealPage(@RequestBody PageParam pageParam) {
         return setmealService.findPage(pageParam);
+    }
+
+
+    /**
+     * 功能描述: 图片上传
+     *
+     * @return : com.baomidou.mybatisplus.extension.api.R
+     */
+    @PostMapping("upload")
+    public R upload(MultipartFile imgFile) {
+
+        if (imgFile == null || imgFile.getSize() == 0) {
+            return R.error(MessageConstant.PIC_UPLOAD_FAIL);
+        }
+        // 获取文件前端传过来的文件名
+        String originalFilename = imgFile.getOriginalFilename();
+        // 获取文件的后缀名
+        String substring = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String prefix = String.valueOf(System.currentTimeMillis());
+        //文件变为字节流
+        InputStream inputStream = null;
+        try {
+            inputStream = imgFile.getInputStream();
+            String url = fileStorageService.uploadImgFile("", prefix + substring, inputStream);
+            return R.success(MessageConstant.PIC_UPLOAD_SUCCESS, url);
+        } catch (IOException e) {
+            return R.error(MessageConstant.PIC_UPLOAD_FAIL);
+        }
     }
 
 }
