de6075eebe07da11440b55d0a094f1bbb7d6bcb0
qingyouyu <19863628661@163.com>
2023-07-27 16:14:48 +0800

    项目第二天-套餐列表展示

--- health_admin/src/main/java/com/itheima/health/controller/SetmealController.java
+++ health_admin/src/main/java/com/itheima/health/controller/SetmealController.java
@@ -1,6 +1,12 @@
 package com.itheima.health.controller;
 
+import com.itheima.health.common.PageParam;
+import com.itheima.health.common.ResultPageData;
+import com.itheima.health.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/setmeal")
 public class SetmealController {
+    @Autowired
+    private SetmealService setmealService;
+
+    /**
+     * 功能描述: 套餐模块分页查询
+     *
+     * @return : com.itheima.health.common.ResultPageData
+     */
+    @PostMapping("/findPage")
+    public ResultPageData findSetmealPage(@RequestBody PageParam pageParam) {
+        return setmealService.findPage(pageParam);
+    }
+
 }
--- health_admin/src/main/java/com/itheima/health/service/SetmealService.java
+++ health_admin/src/main/java/com/itheima/health/service/SetmealService.java
@@ -1,9 +1,52 @@
 package com.itheima.health.service;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.health.common.PageParam;
+import com.itheima.health.common.ResultPageData;
+import com.itheima.health.mapper.SetmealMapper;
+import com.itheima.health.model.pojos.CheckGroup;
+import com.itheima.health.model.pojos.Setmeal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 @Slf4j
 public class SetmealService {
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    /**
+     * 功能描述: 分页查询
+     *
+     * @param pageParam
+     * @return : com.itheima.health.common.ResultPageData
+     */
+    public ResultPageData findPage(PageParam pageParam) {
+        Integer pageSize = pageParam.getPageSize();
+        Integer currentPage = pageParam.getCurrentPage();
+
+        Page<Setmeal> setmealPage = new Page<>(currentPage, pageSize);
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+
+        if (!StringUtils.isEmpty(pageParam.getQueryString())) {
+            if (pageParam.getQueryString().matches("^[0-9]*$")) {
+                queryWrapper.like(Setmeal::getCode, pageParam.getQueryString());
+            } else if (pageParam.getQueryString().matches("^[A-Z]*$")) {
+                queryWrapper.like(Setmeal::getHelpCode, pageParam.getQueryString());
+            } else {
+                queryWrapper.like(Setmeal::getName, pageParam.getQueryString());
+            }
+        }
+        Page page = setmealMapper.selectPage(setmealPage, queryWrapper);
+
+        ResultPageData resultPageData = new ResultPageData();
+        resultPageData.setRows(page.getRecords());
+        resultPageData.setTotal(page.getTotal());
+        return resultPageData;
+    }
 }
